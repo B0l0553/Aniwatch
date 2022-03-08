a1ff9aa58ac52713f4030e26fd55843c8dc23f05
Elven Loret <elvenloret4@gmail.com>
2022-03-09 00:44:31 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import tw from 'tailwind-react-native-classnames';
 
 export default function App() {
+  const [count, setCount] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>You clicked {count} times</Text>
+      <Button onPress={() => setCount(count + 1)} title="Click me!"></Button>
       <StatusBar style="auto" />
     </View>
   );
