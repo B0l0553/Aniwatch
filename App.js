dc860d4d64bcbe6f8f5b187c0d7eed52a38fec64
Elven Loret <elvenloret4@gmail.com>
2022-03-09 19:07:12 +0100

    Initial commit
    Generated by create-react-native-app 3.8.0.

--- App.js
+++ App.js
@@ -1,23 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import tw from 'tailwind-react-native-classnames';
+import { SafeAreaView, Text, View } from "react-native";
+import tailwind from "tailwind-rn";
 
 export default function App() {
-  const [count, setCount] = React.useState(0);
   return (
-    <View style={[tw`flex`]}>
-      <StatusBar style={[]} />
-      <Header />
-    </View>
+    <SafeAreaView style={tailwind("flex-1 items-center justify-center")}>
+      <View style={tailwind("bg-blue-500 px-5 py-3 rounded-full")}>
+        <Text style={tailwind("text-white font-semibold text-lg")}>
+          Hello Tailwind 👋
+        </Text>
+      </View>
+    </SafeAreaView>
   );
-}
-
-function Header() {
-  return (
-    <View style={[tw`flex top-0 bg-blue-600 h-32 w-full`]}>
-      <Text style={[tw`font-extrabold text-3xl font-black tracking-wider text-white my-14`]}>ANIWATCH</Text>
-    </View>
-  )
 }
