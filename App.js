0f7da2d7e18983a2d1948e4ba3d76603568f5ec0
Elven Loret <elvenloret4@gmail.com>
2022-03-09 18:11:53 +0100

    Update App.js

--- App.js
+++ App.js
@@ -5,21 +5,19 @@ import { NavigationContainer } from '@react-navigation/native';
 import tw from 'tailwind-react-native-classnames';
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = React.useState(0);
   return (
-    <View style={styles.container}>
-      <Text>You clicked {count} times</Text>
-      <Button onPress={() => setCount(count + 1)} title="Click me!"></Button>
-      <StatusBar style="auto" />
+    <View style={[tw`flex`]}>
+      <StatusBar style={[]} />
+      <Header />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function Header() {
+  return (
+    <View style={[tw`flex top-0 bg-blue-600 h-32 w-full`]}>
+      <Text style={[tw`font-extrabold text-3xl font-black tracking-wider text-white my-14`]}>ANIWATCH</Text>
+    </View>
+  )
+}
